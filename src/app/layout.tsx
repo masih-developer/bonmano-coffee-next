273ea40fcd7ce22095efdd998798e9588dee247e
masih abedini <masih7816@gmail.com>
2024-06-08 22:14:28 +0330

    💄 (app/layout.tsx): add all font weights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,53 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 const myFont = localFont({
-  src: "../assets/fonts/dana/Dana-Regular.ttf",
+  src: [
+    {
+      path: "../assets/fonts/dana/Dana-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-UltraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-DemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/dana/Dana-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   variable: "--font-dana",
 });
 
