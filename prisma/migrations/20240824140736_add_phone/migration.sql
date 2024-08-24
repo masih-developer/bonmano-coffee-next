a125c9cd0810fff99b99739c40f612b8ea3e46b3
masih abedini <masih7816@gmail.com>
2024-08-24 17:50:22 +0330

    feat(db): add phone field and its migration

---
+++ prisma/migrations/20240824140736_add_phone/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `password` on the `user` table. The data in that column could be lost. The data in that column will be cast from `VarChar(255)` to `VarChar(30)`.
+  - A unique constraint covering the columns `[phone]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `phone` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `phone` VARCHAR(11) NOT NULL,
+    MODIFY `password` VARCHAR(30) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_phone_key` ON `User`(`phone`);
