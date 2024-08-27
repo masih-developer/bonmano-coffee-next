863257ace8679ce2b254d70507bbd5ff70c6fc9c
masih abedini <masih7816@gmail.com>
2024-08-27 22:32:25 +0330

    fix: delete about page

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-
-export default function AboutPage() {
-  const { data: session } = useSession();
-  console.log(session);
-  return <div>AboutPage</div>;
-}
